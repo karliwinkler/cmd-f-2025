18e30f0f9ec6fc219881bb3c06f3fff6e9c4bf1c
Karli Winkler <163181782+karliwinkler@users.noreply.github.com>
2025-03-09 09:48:47 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, redirect, url_for, render_template_string
+import test
+import random
+import nutrition
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search_page")
+def search_page():
+    return render_template("searchPage.htm")
+
+
+@app.route("/recipes_page")
+def recipes_page():
+    cuisine = request.args["cuisine"]
+    category = request.args["category"]
+    ingredient = request.args["ingredient"]
+    print(cuisine+category+ingredient)
+
+    areaRecipes = test.get_recipes_area(cuisine)
+    ingredientRecipes = test.get_recipes_ingredient(ingredient)
+    mainsRecipes = test.get_recipes_mains(category)
+
+    chosen_recipes = random.sample(areaRecipes, 8) + random.sample(ingredientRecipes, 8) + random.sample(mainsRecipes, 8)
+
+
+    return render_template("recipes.html", recipes=chosen_recipes)
+
+@app.route("/recipe/<id>")
+def recipe_detail(id):
+    recipe = test.get_recipe_ID(id)
+    if not recipe:
+        return "Recipe not found", 404
+    return render_template("recipe_detail.html", recipe=recipe[0])
+
+@app.route("/submit_recipes", methods=["POST"])
+def submit_recipes():
+    selected_recipes = request.form.getlist("selected_recipes")  # Get selected checkboxes
+
+    if len(selected_recipes) != 12:
+        return render_template_string("""
+            <script>
+                alert("Error: You must select exactly 12 recipes! You've selected {{sel}}.");
+                window.history.back();  // Goes back to the previous page
+            </script>
+        """, sel=len(selected_recipes))
+
+    return redirect(url_for('meal_plan_page', recipes=selected_recipes))
+
+
+@app.route("/submit_options", methods=['POST'])
+def submit_options():
+    if request.method == 'POST':
+        # Get the selected values from the form
+        cuisine = request.form["cuisine"]
+        ingredient = request.form["ingredient"]
+        category = request.form["category"]
+
+        # Redirect to /results with the selections as URL parameters
+        return redirect(url_for('recipes_page', cuisine=cuisine, ingredient=ingredient, category=category))
+
+    return  "Error", 400
+
+@app.route("/meal_plan_page")
+def meal_plan_page():
+    selected_ids = request.args["recipes"] # these are the IDs
+
+    
+    return render_template("meal_plan.html", recipes=selected_ids)
+
+
+@app.route("/submit_nutrition", methods=['POST'])
+def submit_nutrition():
+    if request.method == 'POST':
+        # Get the selected values from the form
+        
+        age = request.form['age']
+        weight = request.form['weight']
+        height = request.form['height']
+
+        return render_template('meal_plan_submitted_form.html', age=age, weight=weight, height=height)
+
+
+    return  "Error", 400
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
